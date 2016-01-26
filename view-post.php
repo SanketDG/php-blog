2c4dd3ed871473b6e60b7044193dc2e5093d361f
SanketDG <sanketdasgupta@gmail.com>
2016-01-26 22:42:28 +0530

    break body into paragraphs when newline encountered

--- view-post.php
+++ view-post.php
@@ -24,6 +24,8 @@ if($result == false)
 
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
+$bodytext = HTMLEscape($row['body']);
+$paratext = str_replace("\n", "</p><p>", $bodytext)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,7 +46,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
         <?php echo $row['created_at'] ?>
     </div>
     <p>
-        <?php echo HTMLEscape($row['body']) ?>
+        <?php echo $paratext ?>
     </p>
 </body>
 </html>
