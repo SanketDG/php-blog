98c8525cf7ce1dc33703929a1cf2deb617fa7d1a
SanketDG <sanketdasgupta@gmail.com>
2016-01-26 11:54:22 +0530

    add basic template for blog

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>A blog application</title>
+</head>
+<body>
+    <h1>Blog Title</h1>
+    <p>This paragraph summarises what the blog is about.</p>
+
+    <h2>Article 1 title</h2>
+    <div>dd Mon YYYY</div>
+    <p>A paragraph summarising article 1</p>
+    <p><a href="#">Read More</a></p>
+
+    <h2>Article 2 title</h2>
+    <div>dd Mon YYYY</div>
+    <p>A paragraph summarising article 2</p>
+    <p><a href="#">Read More</a></p>
+
+</body>
+</html>
