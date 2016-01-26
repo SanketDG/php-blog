7f7ad2700f5b0f29d33a3fe3f92b776351f29cbe
SanketDG <sanketdasgupta@gmail.com>
2016-01-26 19:53:07 +0530

    use for-endfor syntax to dynamically generate repetitive data

--- index.php
+++ index.php
@@ -5,18 +5,13 @@
     <title>A blog application</title>
 </head>
 <body>
-    <h1>Blog Title</h1>
-    <p>This paragraph summarises what the blog is about.</p>
 
-    <h2>Article 1 title</h2>
-    <div>dd Mon YYYY</div>
-    <p>A paragraph summarising article 1</p>
-    <p><a href="#">Read More</a></p>
-
-    <h2>Article 2 title</h2>
-    <div>dd Mon YYYY</div>
-    <p>A paragraph summarising article 2</p>
-    <p><a href="#">Read More</a></p>
+    <?php for ($postId = 1; $postId <= 3; $postId++): ?>
+        <h2>Article <?php echo $postId ?> title</h2>
+        <div>dd Mon YYYY</div>
+        <p>A paragraph summarising article <?php echo $postId ?>.</p>
+        <p><a href="#">Read more...</a></p>
+    <?php endfor ?>
 
 </body>
 </html>
