09dcf6337c128565191f122d76125fb4c09c802c
SanketDG <sanketdasgupta@gmail.com>
2016-02-13 23:05:20 +0530

    show error for post not found

--- view-post.php
+++ view-post.php
@@ -16,6 +16,13 @@ else
 $pdo = getPDO();
 $row = getPostRow($pdo, $postId);
 
+// If the post does not exist, let's deal with that here
+if (!$row)
+{
+    redirectAndExit('index.php?not-found=1');
+}
+
+
 $bodyText = HTMLEscape($row['body']);
 $paraText = str_replace("\n", "</p><p>", $bodyText)
 ?>
